003a50ff24d3c5517a70465a9063625d8ccef7eb
MedNewton <med.newton.2013@gmail.com>
2023-12-19 18:28:24 +0000

    test push

--- src/components/BuyCMAX.tsx
+++ src/components/BuyCMAX.tsx
@@ -34,7 +34,7 @@ const BuyCMAX = () => {
             aliquip ex ea commodo consequat. Duis aute irure dolor in
             reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            culpa qui officia deserunt.
           </h5>
         </div>
         <div className="flex h-fit w-full flex-row items-center justify-center pt-10 lg:pt-0 lg:w-1/2">
